33226661ad66ad82edb6aca7c3ceeff5d664e903
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2023-07-20 22:04:57 +0530

    Add files via upload

---
+++ hanuan_number.py
@@ -0,0 +1,21 @@
+#numbers in python
+
+print(12)
+print(2+3)
+
+print(2*8+9)
+
+print(2*(8+9))
+num=-8
+print(num)
+
+print(str(num) +  " amarickaa") #numbers of string present in that variables
+
+print(abs(num)) #absolute numbers
+
+#print(str(num)+ pow(2,4) + str(abs(num)) + "ram")
+
+print (round(max(pow(2,9.45),abs(num)))) #find min and max value and also round of the value
+
+#power value is used to separate by comma
+#floor and chil function also available
