b12910b160ae9e4ae2ac3fd7977b6c8c8a21ca6a
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2023-08-15 21:08:30 +0530

    Add files via upload

---
+++ modules/extra.py
@@ -0,0 +1,6 @@
+def function1():
+    print("call function 1")
+def function2():
+    print("call functoin 2")
+def function3():
+    print("call function 3")
---
+++ modules/main.py
@@ -0,0 +1,10 @@
+#call function from other other file using import or from key word
+import extra #import all function from this file
+from extra import function2 #import paticular function by "from"
+from extra import function3 as f3 #'as' key is used for short form for function
+
+
+
+extra.function1()
+function2()
+f3()
