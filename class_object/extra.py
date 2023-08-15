6e8e2f47633ffe2c47f5e7838b9adb59b28dd033
SelvaGeetha <92872009+SelvaGeetha@users.noreply.github.com>
2023-08-15 21:11:01 +0530

    Add files via upload

---
+++ class_object/extra.py
@@ -0,0 +1,11 @@
+##collection of related function and data
+#compulsry give init for function
+class classroon:
+    def __init__(self,name,age,classn):# must start in self
+        self.name = name
+        self.age = age
+        self.classn=classn
+    def print(self):
+        print("name:"+self.name)
+        print("age:"+self.age)
+        print("classrm:"+self.classn)        
---
+++ class_object/main.py
@@ -0,0 +1,7 @@
+from extra import classroon
+
+selva=classroon("sekva","3","45") #create function for object in object name
+
+print(selva.classn)
+
+selva.print() #created variable name 
